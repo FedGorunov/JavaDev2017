6865f5abbb1bf78fcabc1588a050f6917302a817
Fedorsveta <pinta.f14@gmail.com>
2017-06-27 15:51:03 +0700

    в класс QueueContext добавлена аннотация @ComponentScan и удален код создания бинов

--- tracker-core/src/main/java/jdev/tracker/Main.java
+++ tracker-core/src/main/java/jdev/tracker/Main.java
@@ -10,7 +10,7 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
  */
 public class Main {
     public static void main(String[] args) throws Exception {
-        ApplicationContext context = new AnnotationConfigApplicationContext(QieueContext.class);
+        ApplicationContext context = new AnnotationConfigApplicationContext(QueueContext.class);
 
     }
 }
--- tracker-core/src/main/java/jdev/tracker/QueueContext.java
+++ tracker-core/src/main/java/jdev/tracker/QueueContext.java
@@ -1,9 +1,8 @@
 package jdev.tracker;
 
-import jdev.tracker.services.DataSendService;
-import jdev.tracker.services.DataStorageService;
-import jdev.tracker.services.GpsService;
+
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.TaskScheduler;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -14,21 +13,8 @@ import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
  */
 @Configuration
 @EnableScheduling
-class QieueContext {
-    @Bean
-    public DataSendService sendService() {
-        return new DataSendService();
-    }
-
-    @Bean
-    public DataStorageService storageService() {
-        return new DataStorageService();
-    }
-
-    @Bean
-    public GpsService gpsService() {
-        return new GpsService();
-    }
+@ComponentScan
+class QueueContext {
 
     @Bean
     public TaskScheduler poolScheduler() {
