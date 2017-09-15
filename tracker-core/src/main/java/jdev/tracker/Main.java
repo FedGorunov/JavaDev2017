188816e7336cc17a891a4c03536c72b168bdf910
Fedorsveta <pinta.f14@gmail.com>
2017-09-15 16:59:34 +0700

    рефакторинг сервиса gps в tracker-core

--- tracker-core/src/main/java/jdev/tracker/Main.java
+++ tracker-core/src/main/java/jdev/tracker/Main.java
@@ -3,10 +3,7 @@ package jdev.tracker;
 
 
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.*;
 import org.springframework.scheduling.TaskScheduler;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
@@ -17,6 +14,7 @@ import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 @ComponentScan
 @EnableScheduling
 @Configuration
+@PropertySource("classpath:/tracker.properties")
 public class Main {
     public static void main(String[] args) throws Exception {
         ApplicationContext context = new AnnotationConfigApplicationContext(Main.class);
--- tracker-core/src/main/java/jdev/tracker/services/GpsService.java
+++ tracker-core/src/main/java/jdev/tracker/services/GpsService.java
@@ -6,9 +6,11 @@ import jdev.dto.Points;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
+import javax.annotation.PostConstruct;
 import java.io.File;
 import java.util.List;
 
@@ -18,39 +20,78 @@ import java.util.List;
 @Service
 public class GpsService {
     private static final Logger log = LoggerFactory.getLogger(GpsService.class);
-    private int putCount =0;
+    private int poolCount =0;
+    private List<Coordinate> coordinates;
     private static final int TIMEOUT = 1_000;
-    private  long time = 1_498_998_207_587L;
-    double lat, latPrev, lon, lonPrev;
-    @Autowired
-    DataStorageService dataStorageService;
+    private  long time;
+    double lat =0, latPrev=0, lon=0, lonPrev=0;
 
-    final Kml kml = Kml.unmarshal(new File("tracker-core//src//main//resources//26521.kml"));
-    final Placemark placemark = (Placemark) kml.getFeature();
-    LineString point = (LineString) placemark.getGeometry();
-    List<Coordinate> coordinates = point.getCoordinates();
+
+    private final DataStorageService dataStorageService;
+
+    public GpsService(@Autowired DataStorageService dataStorageService) {
+        this.dataStorageService = dataStorageService;
+    }
+
+    @Value("${autoNum}")
+    private String autoId;
+
+    @Value("${kmlFileName}")
+    private String kmlFileName;
+
+
+
+    @PostConstruct
+    private void init(){
+        coordinates = createCoordinates(kmlFileName);
+    }
 
     @Scheduled(fixedRate = TIMEOUT)
-    public void poolGPS() throws InterruptedException {
-        int i = putCount++;
-        if (i < coordinates.size()) {
+    public void poolGPS() {
+        if (coordinates.iterator().hasNext()) {
+            Coordinate coordinate = coordinates.iterator().next();
             PointDTO dto = new PointDTO();
-            dto.setAutoId("a001aa");
-            latPrev = coordinates.get(i - 1).getLatitude();
-            lat = coordinates.get(i).getLatitude();
-            lonPrev = coordinates.get(i - 1).getLongitude();
-            lon = coordinates.get(i).getLongitude();
-
-            dto.setLat(latPrev);
-            dto.setLon(lonPrev);
-            time += TIMEOUT;
+            dto.setAutoId(autoId);
+            time= System.currentTimeMillis();
             dto.setTime(time);
+            lat = coordinate.getLatitude();
+            lon = coordinate.getLongitude();
+            dto.setLat(lat);
+            dto.setLon(lon);
             dto.setCourse(Points.getCourse(latPrev, lat, lonPrev, lon));
             dto.setSpeed(Points.getDistance(latPrev, lat, lonPrev, lon) * 1000 / TIMEOUT);
-            dataStorageService.put(dto);
+            try {
+                dataStorageService.put(dto);
+                log.info("Gps point num: " + poolCount + " Time " + time);
+                coordinates.remove(coordinate);
+                latPrev =lat;
+                lonPrev =lon;
+                poolCount++;
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
         } else {
             log.info("The track is over.");
 
         }
+    }
+
+    public List<Coordinate> createCoordinates(String kmlFile){
+        final Kml kml = Kml.unmarshal(new File(getClass().getClassLoader().getResource(kmlFile).getFile()));
+        final Placemark placemark = (Placemark) kml.getFeature();
+        LineString point = (LineString) placemark.getGeometry();
+        return  point.getCoordinates();
+    }
+
+    public void setCoordinates(List<Coordinate> coordinates) {
+        this.coordinates = coordinates;
+    }
+
+    public void setAutoId(String autoId) {
+        this.autoId = autoId;
+    }
+
+    public List<Coordinate> getCoordinates() {
+        return coordinates;
     }
 }
